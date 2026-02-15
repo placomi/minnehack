4779066615d5bcb825805315cd566ed4d597e331
Michael Zewdie <michaelzewdie7@gmail.com>
2026-02-14 22:48:25 -0600

    Got json parsed

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -3,7 +3,8 @@ import llm_analysis as llm_analysis
 import json
 import Snippet 
 
-df = pd.read_csv('/Users/michaelzewdie/CS/Hackathon/minnehack/web/src/app/api/test.csv')
+
+df = pd.read_csv('/Users/michaelzewdie/CS/Hackathon/minnehack/scraper/test.csv')
 # for tweet in df:
 #     llm_analysis = llm_analysis(tweet)
 #     snippet = snippet(
@@ -18,9 +19,10 @@ json = json.loads(raw)
 summary = json['summary']
 tags = ['tags']
 location = json['location']
-tradegy = json['Tradegy']
-print(json['summary'], json['tags'], json['location'], json['Tradegy'])
-snippet = Snippet(timestamp=1.0,summary=summary, text=text, created_by='today',verified=True,lat=1.00,Long=9.0,tags=[result['tags']])
+tradegy = json['tragedy']
+snippet = Snippet.Snippet(timestamp=1.0,summary=summary, text=text, created_by='today',verified=True,lat=1.00,Long=9.0,tags=tags)
+print(snippet)
+print(location)
 
 
 
