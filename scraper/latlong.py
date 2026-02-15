f61afd650eb0088b2ef5c83d162ba34f80e5e8f1
Michael Zewdie <michaelzewdie7@gmail.com>
2026-02-14 22:54:12 -0600

    lat and long added

---
+++ scraper/latlong.py
@@ -0,0 +1,8 @@
+from geopy.geocoders import Nominatim
+
+def latlong(location): 
+    geolocator = Nominatim(user_agent="minnehack")
+    location = geolocator.geocode("China")
+    return[location.latitude, location.longitude]
+
+latlong('china')
